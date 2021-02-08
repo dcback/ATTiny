eb052aa801f440a2364f3ae678c09cc68804074f
DC Back <45322614+dcback@users.noreply.github.com>
2021-02-09 05:38:14 +0900

    Update Blink.ino

--- Blink.ino
+++ Blink.ino
@@ -1,37 +1,35 @@
-/* http://homemadehardware.com/img/attiny85_pinout.jpeg
+/*
   [Attiny 25/45/85 Pinout]
                   ------
   5=PB5=A0=RESET-|      |-VCC
         3=PB3=A3-|      |-2=PB2=A1=SCK
         4=PB4=A2-|      |-1=PB1=MISO
-            GND -|      |-0=PB0=MOSI
+            GND -|      |-0=PB0=MOSI=SDA
                   ------
 */
 #include <SoftwareSerial.h>
 
-#define rxPin       4     // PB4
-#define txPin       3     // PB3
-#define ledPin      1     // PB1
+#define txPin       0     // PB0
+#define rxPin       1     // PB1
+#define ledPin      1     // PB1 can be used rxPin & ledPin
+
+bool  ledState = 0;
 
 SoftwareSerial mySerial(rxPin, txPin);
 
-bool ledState = 0;
-
 void setup()
 {
+  mySerial.begin(9600);   // Max speed:9600
+  mySerial.println("[LED On/Off in 1 sec.]");
   pinMode(ledPin, OUTPUT);
-  mySerial.begin(9600);
-  mySerial.println("[ATtiny LED Blink]");
 }
 
 void loop()
 {
   digitalWrite(ledPin, ledState);
-  
   mySerial.print("LED-State : ");
   mySerial.println(ledState);
+  delay(1000);
 
   ledState = !ledState;
-
-  delay(1000);
 }
