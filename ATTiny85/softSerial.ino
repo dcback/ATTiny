f03299664ec4c3bd0951734473cbd9199ee23c1a
DC Back <45322614+dcback@users.noreply.github.com>
2021-02-09 06:51:08 +0900

    Add files via upload

---
+++ ATTiny85/softSerial.ino
@@ -0,0 +1,26 @@
+/***
+  [Attiny 25/45/85 Pinout]
+                  ------
+  RESET=A0=PB5=5-|      |-VCC
+        A3=PB3=3-|      |-2=PB2=A1=SCK=SCL
+        A2=PB4=4-|      |-1(~)=PB1=MISO
+             GND-|      |-0(~)=PB0=MOSI=SDA
+                  ------
+***/
+#include <SoftwareSerial.h>
+
+#define txPin       0     // PB0
+#define rxPin       1     // PB1
+
+SoftwareSerial mySerial(rxPin, txPin);
+
+void setup()
+{
+  mySerial.begin(9600);   // Max speed : 9600
+}
+
+void loop()
+{
+  mySerial.println("[ATTiny85 Serial Print Test]");
+  delay(1000);
+}
